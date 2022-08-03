c0f2cd7ae53a95b586a5d3f240475270a64f384f
CHA-HK <chku52@gmail.com>
2022-08-03 22:47:49 +0900

    2022.08.03

--- class/src/day0803/BOJ11660.java
+++ class/src/day0803/BOJ11660.java
@@ -8,16 +8,39 @@ import java.util.StringTokenizer;
 public class BOJ11660 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()); // 표의 크기
-		int M = Integer.parseInt(br.readLine()); // 합 구하는 횟수
-		int[][] map = new int[N][N];
+		StringBuilder sb = new StringBuilder();
 		
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < N; j++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); // 표의 크기
+		int M = Integer.parseInt(st.nextToken()); // 합 구하는 횟수
+		int[][] map = new int[N + 1][N + 1];
+		
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
+		int[][] sum = new int[N + 1][N + 1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				sum[i][j] = sum[i - 1][j] + sum[i][j - 1] + map[i][j] - sum[i - 1][j - 1];
+			}
+		}
+		
+		int result = 0;
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int startX = Integer.parseInt(st.nextToken()); // 시작
+			int startY = Integer.parseInt(st.nextToken());
+			int endX = Integer.parseInt(st.nextToken()); // 끝
+			int endY = Integer.parseInt(st.nextToken());
+			
+			result = sum[endX][endY] - sum[startX - 1][endY] - sum[endX][startY - 1] + sum[startX - 1][startY - 1];
+			sb.append(result).append('\n');
+		}
+		
+		System.out.println(sb);
 	}
 }
--- class/src/day0803/SWEA2001.java
+++ class/src/day0803/SWEA2001.java
@@ -5,20 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class SWEA2001 {
+public class SWEA2001 { // 파리 퇴치
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int t = 1; t <= T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int[][] map = new int[N][M];
+			int N = Integer.parseInt(st.nextToken()); // 배열 크기
+			int M = Integer.parseInt(st.nextToken()); // 파리채 크기
+			int[][] map = new int[N][N];
 			
 			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < M; j++) {
+				for(int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
@@ -27,9 +27,17 @@ public class SWEA2001 {
 			for(int i = 0; i <= N - M; i++) {
 				for(int j = 0; j <= N - M; j++) {
 					int sum = 0;
-					sum += map[i][j];
+					for(int x = 0; x < M; x++) {
+						for(int y = 0; y < M; y++) {
+							sum += map[i + x][j + y];
+						}
+					}
+					
+					max = Math.max(max, sum);
 				}
 			}
+			
+			System.out.println("#" + t + " " + max);
 		}
 	}
 }
