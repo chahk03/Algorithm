669e2251b9865d739dac7f071d8b6c99e44f3a5f
chahk03 <chku52@gmail.com>
2022-09-08 03:10:50 +0900

    2022.09.08

--- study/src/week16/BOJ2917.java
+++ study/src/week16/BOJ2917.java
@@ -3,16 +3,18 @@ package week16;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ2917 { // 늑대 사냥꾼
 	static int N, M;
-	static int[][] map;
+	static char[][] map;
+	static int[][] dist;
 	static boolean[][] visit;
-	static int startX, startY;
-	static int endX, endY;
-	static ArrayList<Node> trees;
+	static int startX, startY, endX, endY;
+	static Queue<Node> tree;
 	static int[] dx = {0, 1, 0, -1};
 	static int[] dy = {1, 0, -1, 0};
 	static int max;
@@ -23,78 +25,98 @@ public class BOJ2917 { // 늑대 사냥꾼
 		
 		N = Integer.parseInt(st.nextToken()); // 행
 		M = Integer.parseInt(st.nextToken()); // 열
-		map = new int[N][M];
+		map = new char[N][M];
+		dist = new int[N][M];
 		visit = new boolean[N][M];
-		trees = new ArrayList<>();
+		tree = new ArrayDeque<>();
 		
 		for(int i = 0; i < N; i++) {
 			String str = br.readLine();
 			for(int j = 0; j < M; j++) {
-				char value = str.charAt(j);
+				map[i][j] = str.charAt(j);
+				dist[i][j] = Integer.MAX_VALUE;
 				
-				if(value == 'V') {
-					startX = i;
-					startY = j;
+				if(map[i][j] == 'V') {
+					startX = i; startY = j;
 				}
 				
-				if(value == 'J') {
-					endX = i;
-					endY = j;
+				if(map[i][j] == 'J') {
+					endX = i; endY = j;
 				}
 				
-				if(value == '+') {
-					trees.add(new Node(i, j));
+				if(map[i][j] == '+') {
+					dist[i][j] = 0;
+					tree.add(new Node(i, j));
 				}
 			}
 		}
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				int min = Integer.MAX_VALUE;
-				for(Node tree : trees) {
-					int dist = Math.abs(tree.x - i) + Math.abs(tree.y - j);
-					min = Math.min(min, dist);
-				}
-				map[i][j] = min;
-			}
-		}
+		bfs();
 		
-		for(int i = map[startX][startY]; i >= 1; i--) {
-			dfs(startX, startY, i);
-			if(max == i) break;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(startX, startY));
+		visit[startX][startY] = true;
+		
+		while(!pq.isEmpty()) {
+			Node n = pq.poll();
+				
+			if(n.x == endX && n.y == endY) {
+				max = dist[n.x][n.y];
+				break;
+			}
+			
+			for(int d = 0; d < 4; d++) {
+				int nx = n.x + dx[d];
+				int ny = n.y + dy[d];
+					
+				if(0 <= nx && nx < N && 0 <= ny && ny < M) {
+					if(dist[nx][ny] > dist[n.x][n.y]) {
+						dist[nx][ny] = dist[n.x][n.y];
+					}
+					
+					if(!visit[nx][ny]) {
+						visit[nx][ny] = true;
+						pq.add(new Node(nx, ny));
+					}
+				}
+			}
 		}
 		
 		System.out.println(max);
 	}
 	
-	static void dfs(int x, int y, int v) {
-		visit[x][y] = true;
-
-		if(x == endX && y == endY) {
-			max = Math.max(max,  v);
-			return;
-		}
-		
-		for(int d = 0; d < 4; d++) {
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			
-			if(0 <= nx && nx < N && 0 <= ny && ny < M && !visit[nx][ny]) {
-				if(map[nx][ny] >= v) {
-					dfs(nx, ny, v);
-				}
+	static void bfs() {
+		while(!tree.isEmpty()) {
+			Node n = tree.poll();
 				
-				visit[nx][ny] = false;
+			for(int d = 0; d < 4; d++) {
+				int nx = n.x + dx[d];
+				int ny = n.y + dy[d];
+					
+				if(0 <= nx && nx < N && 0 <= ny && ny < M) {
+					if(map[nx][ny] != '+') {
+						if(dist[nx][ny] > dist[n.x][n.y] + 1) {
+							dist[nx][ny] = dist[n.x][n.y] + 1;
+							tree.add(new Node(nx, ny));
+						}
+					}
+				}
 			}
 		}
 	}
 	
-	static class Node {
-		int x, y;
+	static class Node implements Comparable<Node> {
+		int x, y, w;
 		
 		Node(int x, int y) {
 			this.x = x;
 			this.y = y;
+			this.w = dist[x][y];
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return o.w - this.w;
 		}
 	}
 }
