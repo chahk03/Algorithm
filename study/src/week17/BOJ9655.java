6d55bdfd03328cd7aee3fe1b9ae866fa2339ee08
chahk03 <chku52@gmail.com>
2022-09-14 21:17:48 +0900

    2022.09.14

--- study/src/week17/BOJ9655.java
+++ study/src/week17/BOJ9655.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ9655 {
+public class BOJ9655 { // 돌 게임
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
