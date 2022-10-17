15261c5ffc0136fd705d3ec041e25a6a050a60b8
chahk03 <chku52@gmail.com>
2022-10-18 01:29:38 +0900

    2022.10.18

--- study/src/week12/BOJ14225_뭐지.java
+++ study/src/week12/BOJ14225_뭐지.java
@@ -3,18 +3,22 @@ package week12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ14225 { // 부분수열의 합
+public class BOJ14225_뭐지 { // 부분수열의 합
 	static int N;
 	static int[] nums;
+	static boolean[] select;
 	static boolean[] sums;
+	static int result;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		
 		nums = new int[N];
+		select = new boolean[N];
 		int total = 0;
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,24 +27,36 @@ public class BOJ14225 { // 부분수열의 합
 			total += nums[i];
 		}
 		
-		sums = new boolean[total + 2];
+		sums = new boolean[total + 1];
+		result = total + 1;
 		findSum(0, 0);
 		
-		for(int i = 1; i <= total + 1; i++) {
+		for(int i = 1; i <= total; i++) {
 			if(!sums[i]) {
-				System.out.println(i);
+				result = i;
 				break;
 			}
 		}
+		
+		System.out.println(result);
 	}
 
-	private static void findSum(int idx, int sum) {
-		if(idx == N) {
+	private static void findSum(int cnt, int start) {
+		if(cnt == N) {
+			int sum = 0;
+			for(int i = 0; i < N; i++) {
+				if(select[i]) sum += nums[i];
+			}
+			
 			sums[sum] = true;
 			return;
 		}
 		
-		findSum(idx + 1, sum + nums[idx]);
-		findSum(idx + 1, sum);
+		for(int i = start; i < N; i++) {
+			select[i] = true;
+			findSum(cnt + 1, i + 1);
+			select[i] = false;
+			findSum(cnt + 1, i + 1);
+		}		
 	}
 }
--- study/src/week12/BOJ14225_시간초과.java
+++ study/src/week12/BOJ14225_시간초과.java
@@ -3,12 +3,14 @@ package week12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ14225 { // 부분수열의 합
+public class BOJ14225_시간초과 { // 부분수열의 합
 	static int N;
 	static int[] nums;
 	static boolean[] sums;
+	static int result;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,24 +25,31 @@ public class BOJ14225 { // 부분수열의 합
 			total += nums[i];
 		}
 		
-		sums = new boolean[total + 2];
-		findSum(0, 0);
+		sums = new boolean[total + 1];
+		result = total + 1;
+		findSum(0, 0, 0);
 		
-		for(int i = 1; i <= total + 1; i++) {
+		for(int i = 1; i <= total; i++) {
 			if(!sums[i]) {
-				System.out.println(i);
+				result = i;
 				break;
 			}
 		}
+		
+		System.out.println(result);
 	}
 
-	private static void findSum(int idx, int sum) {
-		if(idx == N) {
-			sums[sum] = true;
+	private static void findSum(int cnt, int start, int sum) {
+		sums[sum] = true;
+		// System.out.println(Arrays.toString(sums));
+		
+		if(cnt == N) {
 			return;
 		}
 		
-		findSum(idx + 1, sum + nums[idx]);
-		findSum(idx + 1, sum);
+		for(int i = start; i < N; i++) {
+			findSum(cnt + 1, i + 1, sum + nums[i]);
+			findSum(cnt + 1, i + 1, sum);
+		}		
 	}
 }
