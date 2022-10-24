9b32f530b0691167ce465d4c7f5380b1c547b458
chahk03 <chku52@gmail.com>
2022-10-25 00:33:19 +0900

    [BOJ] 14267. 회사 문화1

--- study/src/week13/BOJ14267.java
+++ study/src/week13/BOJ14267.java
@@ -13,38 +13,30 @@ public class BOJ14267 { // 회사 문화1
 		
 		int n = Integer.parseInt(st.nextToken()); // 회사 직원 수
 		int m = Integer.parseInt(st.nextToken()); // 최초 칭찬 횟수
-		int[] boss = new int[n + 1];
-		int[] cnt = new int[n + 1];
-		int[] dp = new int[n + 1];
 		
+		int[] boss = new int[n + 1];
+		int[] dp = new int[n + 1];
+
 		st = new StringTokenizer(br.readLine());
 		for(int i = 1; i <= n; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			if(num == -1) num = 0;
-			boss[num] = i;
+			if(num == -1) continue;
+			boss[i] = num;
 		}
 		
 		for(int x = 0; x < m; x++) {
 			st = new StringTokenizer(br.readLine());
 			int i = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			cnt[i] = w;
-		} // end input
-		
-		int sum = 0;
-		for(int i = 1; i <= n; i++) {
-			int w = cnt[i];
-			dp[i] = sum + w;
-			sum += w;
+			dp[i] += w;
 		}
 		
-		int num = 0;
 		for(int i = 1; i <= n; i++) {
-			if(dp[i] != 0 && dp[i] != num) {
-				num = dp[i];
-			}
-			
-			sb.append(num + " ");
+			dp[i] += dp[boss[i]];
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			sb.append(dp[i] + " ");
 		}
 		
 		System.out.println(sb);
