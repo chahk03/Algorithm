06106f710073f4084ef335b7b52866805ced0403
chahk03 <chku52@gmail.com>
2022-10-25 23:31:04 +0900

    [BOJ] 2607. 비슷한 단어

--- study/src/week13/BOJ2607.java
+++ study/src/week13/BOJ2607.java
@@ -13,35 +13,46 @@ public class BOJ2607 { // 비슷한 단어
 		
 		String word = br.readLine();
 		for(int i = 0; i < word.length(); i++) {
-			char chr = word.charAt(i);
-			alpha[chr - 65] += 1;
+			alpha[word.charAt(i) - 65] += 1;
 		}	
 		
 		int result = 0;
+		int length = word.length();
+
 		for(int i = 1; i < n; i++) {
 			int change = 0;
-			boolean flag = true;
 			int[] alpha_copy = alpha.clone();
+			
 			word = br.readLine();
 			
-			for(int j = 0; j < word.length(); j++) {
-				char chr = word.charAt(j);
-				if(alpha_copy[chr - 65] == 0) {
-					if(change == 0) {
-						change += 1;						
+			if(Math.abs(word.length() - length) > 1) continue;
+			
+			// 한 문자를 더한 경우
+			// 같은 구성이거나 하나의 문자를 다른 문자로 바꾼 경우
+			if(word.length() >= length) {
+				for(int j = 0; j < word.length(); j++) {
+					if(alpha_copy[word.charAt(j) - 65] > 0) {
+						alpha_copy[word.charAt(j) - 65] -= 1;
 					} else {
-						flag = false;
+						change += 1;
+					}
+				}
+			}
+
+			if(word.length() < length) { // 한 문자를 뺀 경우
+				for(int j = 0; j < word.length(); j++) {
+					if(alpha_copy[word.charAt(j) - 65] > 0) {
+						alpha_copy[word.charAt(j) - 65] -= 1;
+					} else {
+						change = 2;
 						break;
 					}
-				} else {
-					alpha_copy[chr - 65] -= 1;
 				}
 			}
 			
-			if(flag) result++;
+			if(change <= 1) result++;
 		}
 		
 		System.out.println(result);
 	}
-
 }
