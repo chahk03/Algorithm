ab5ffc3bb9db5cb1e0434832fe87e5cef87e656d
chahk03 <chku52@gmail.com>
2022-11-14 21:05:11 +0900

    2022.11.13

---
+++ study/src/week15/BOJ2688.java
@@ -0,0 +1,19 @@
+package week15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2688 { // 줄어들지 않아
+	static int n;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= T; t++) {
+			n = Integer.parseInt(br.readLine());
+			
+		}
+	}
+}
