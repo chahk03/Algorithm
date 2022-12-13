ace4876abdf75cf59698a911b7299406598a2fe1
HyunKyoung Cha <77661312+chahk03@users.noreply.github.com>
2022-12-14 05:38:40 +0900

    [level 2] Title: 프린터, Time: 1.15 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -0,0 +1,40 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+                
+        for(int i = 0; i < priorities.length; i++) {
+            queue.add(i);
+        }
+        
+        while(true) {
+            int doc = queue.poll();
+            boolean print = true;
+            
+            Queue<Integer> queue_copy = new ArrayDeque<>(queue);
+            int size = queue.size();
+            
+            for(int i = 0; i < size; i++) {
+                int next_doc = queue.poll();
+                
+                if(priorities[doc] < priorities[next_doc]) {
+                    queue_copy.add(doc);
+                    print = false;
+                    break;
+                }
+            }
+            
+            queue = queue_copy;
+            
+            if(print) {
+                answer++;
+                if(location == doc) break;
+            }
+        }
+        
+        return answer;
+    }
+}
