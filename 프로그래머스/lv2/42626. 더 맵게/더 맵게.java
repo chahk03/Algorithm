d9fc2ff46035cb002af6cf149825301c0650d173
HyunKyoung Cha <77661312+chahk03@users.noreply.github.com>
2022-12-22 07:00:53 +0900

    [level 2] Title: 더 맵게, Time: 1882.86 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -5,22 +5,22 @@ class Solution {
         int answer = 0;
         int new_scoville = 0;
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 오름차순 정렬
         for(int i = 0; i < scoville.length; i++) {
             pq.add(scoville[i]);
         }
         
         while(true) {
-            if(pq.size() == 1) {
+            if(pq.size() == 1) { // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우
                 answer = -1;
                 break;
             }
             
             new_scoville = pq.poll() + 2 * pq.poll();
-            pq.add(new_scoville);
-            answer++;
+            pq.add(new_scoville); // 새로운 음식의 스코빌 지수 add
+            answer++; // 섞은 횟수 1 증가
             
-            if(pq.peek() >= K) break;
+            if(pq.peek() >= K) break; // 최소 스코빌 지수가 K 이상인 경우 while문 종료
         }
         
         return answer;
