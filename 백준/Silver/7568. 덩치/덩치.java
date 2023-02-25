cf9b000201a36390320230afb7d870629820879e
HyunKyoung Cha <77661312+chahk03@users.noreply.github.com>
2023-02-26 04:18:09 +0900

    [Silver V] Title: 덩치, Time: 140 ms, Memory: 15696 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,46 +1,33 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[][] info = new int[N][2];
-		
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			info[i][0] = Integer.parseInt(st.nextToken());
-			info[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < N; i++) {
-			int rank = 0;
-			
-			int w = info[i][0];
-			int h = info[i][1];
-			
-			for(int j = 0; j < N; j++) {
-				if(j == i)
-					continue;
-				
-				if((info[j][0] > w) && (info[j][1] > h))
-					rank++;
-			}
-			
-			sb.append((rank + 1) + " ");
-		}
-		
-		System.out.println(sb);
+        int N = Integer.parseInt(br.readLine());
+        int[][] info = new int[N][2];
 
-	}
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            info[n][0] = Integer.parseInt(st.nextToken());
+            info[n][1] = Integer.parseInt(st.nextToken());
+        }
 
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+            for (int j = 0; j < N; j++) {
+                if(info[i][0] < info[j][0] && info[i][1] < info[j][1]) {
+                    rank++;
+                }
+            }
+
+            sb.append(rank + " ");
+        }
+
+        System.out.println(sb);
+    }
 }
