099ef41d146a4736b93d6d5c2008eff38e6ad52d
HyunKyoung Cha <77661312+chahk03@users.noreply.github.com>
2023-05-31 16:20:03 +0900

    [Silver II] Title: 다각형그리기, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/2641. 다각형그리기/다각형그리기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); //표본 모양수열의 길이
+        String origin = br.readLine();
+
+        for (int i = 0; i < N; i++) {
+            origin = origin.replace(" ", "");
+        }
+
+        //순 방향으로 (수열 두번 합치기)
+        String forward = origin + origin;
+
+        //반대 방향으로 (숫자 변환한 뒤 방향 바꾸기)
+        String backward = "";
+        int[] dir = {0, 3, 4, 1, 2};
+        for (int i = 0; i < N; i++) {
+            int num = dir[origin.charAt(i) - '0'];
+            backward = String.valueOf(num) + backward;
+        }
+
+        backward = backward + backward;
+
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine()); //모양수열의 개수
+
+        for (int i = 0; i < M; i++) {
+            String seq = br.readLine();
+            String input = seq.replace(" ", "");
+            if (forward.contains(input) || backward.contains(input)) {
+                count++;
+                sb.append(seq).append("\n");
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(sb);
+    }
+}
