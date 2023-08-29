fd6a91cae6c8cccfafdbd1324d44c9185c4aa3e7
HyunKyoung Cha <77661312+chahk03@users.noreply.github.com>
2023-08-29 22:52:59 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 3.17 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,11 +1,9 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {        
-        String[] date = today.split("\\.");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int nowDay = getDay(today);
         
         // 약관 종류별 유효기관 입력
         int[] termArr = new int[26];
@@ -15,45 +13,20 @@ class Solution {
         }
         
         // 개인정보별 유효기간 확인
-        ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < privacies.length; i++) {
-            String[] splitPriv = privacies[i].split(" ");
-            String[] privDate = splitPriv[0].split("\\.");
-            
-            int privYear = Integer.parseInt(privDate[0]);
-            int privMonth = Integer.parseInt(privDate[1]);
-            int privDay = Integer.parseInt(privDate[2]);
-            
-            int plusMonth = termArr[splitPriv[1].charAt(0) - 'A'];            
-            int endYear = privYear + plusMonth / 12; int endDay = privDay - 1;
-            int endMonth = privMonth + plusMonth - 12 * (plusMonth / 12);
-            
-            if(endMonth > 12) {
-                endYear += 1;
-                endMonth -= 12;
-            }
-            
-            if(endDay < 1) {
-                endMonth -= 1;
-                endDay = 28;
-                
-                if(endMonth < 1) {
-                    endYear -= 1;
-                    endMonth = 12;
-                }
-            }
-            
-            if(endYear < year || (endYear <= year && endMonth < month) || (endYear <= year && endMonth <= month && endDay < day)) {
-                list.add(i + 1);
-            }
-        }
-               
-        int idx = 0;
-        int[] answer = new int[list.size()];
-        for(int num : list) {
-            answer[idx++] = num;
+            String[] privacy = privacies[i].split(" ");
+            int privDay = getDay(privacy[0]) + termArr[privacy[1].charAt(0) - 'A'] * 28;
+            if(privDay <= nowDay) answer.add(i + 1);
         }
         
-        return answer;
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+    
+    private int getDay(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
