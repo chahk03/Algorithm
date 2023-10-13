a36ef66ee45fbc8d8a48cea9d7a856a0dac7559d
HyunKyoung Cha <77661312+chahk03@users.noreply.github.com>
2023-10-14 03:07:58 +0900

    [level 3] Title: 여행경로, Time: 11.76 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,53 +1,30 @@
 import java.util.*;
 
 class Solution {
-    HashMap<String, List<String>> map;
-    HashMap<String, Integer> visited;
+    boolean[] visited;
     List<String> results;
     
     public String[] solution(String[][] tickets) {
-        map = new HashMap<>();
-        visited = new HashMap<>();
-        
-        for(String[] ticket : tickets) {
-            visited.put(ticket[0] + ticket[1], visited.getOrDefault(ticket[0] + ticket[1], 0) + 1);
-            if(map.get(ticket[0]) == null) {
-                List<String> list = new ArrayList<>();
-                list.add(ticket[1]);
-                map.put(ticket[0], list);
-            } else {
-                map.get(ticket[0]).add(ticket[1]);
-            }
-        } // end input        
-        
+        visited = new boolean[tickets.length];
         results = new ArrayList<>();
-        dfs(tickets.length, "ICN", "ICN");
+        dfs(0, "ICN", "ICN", tickets);
         
         Collections.sort(results);
-        String result = results.get(0);
-        String[] answer = new String[result.length() / 3];
-        for(int i = 0, idx = 0; i < result.length(); i += 3) {
-            answer[idx++] = result.substring(i, i + 3);
-        }
-        
+        String[] answer = results.get(0).split(" ");        
         return answer;
     }
     
-    private void dfs(int cnt, String start, String info) {
-        if(cnt == 0) {
+    private void dfs(int cnt, String start, String info, String[][] tickets) {
+        if(cnt == tickets.length) {
             results.add(info);
             return;
         }        
         
-        if(map.get(start) != null) {
-            int size = map.get(start).size();
-            for(int i = 0; i < size; i++) {
-                String next = map.get(start).get(i);
-                if(visited.get(start + next) > 0) {
-                    visited.put(start + next, visited.get(start + next) - 1);
-                    dfs(cnt - 1, next, info + next);
-                    visited.put(start + next, visited.get(start + next) + 1);
-                }
+        for(int i = 0; i < tickets.length; i++) {
+            if(!visited[i] && tickets[i][0].equals(start)) {
+                visited[i] = true;
+                dfs(cnt + 1, tickets[i][1], info + " " + tickets[i][1], tickets);
+                visited[i] = false;
             }
         }
     }
